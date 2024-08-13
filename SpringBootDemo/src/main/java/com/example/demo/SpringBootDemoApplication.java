83d074b6d0a2d47f95a017d04cae665048f6490a
AQ00857262 <AQ00857262@INHYIZLP31711.TechMahindra.com>
2024-08-13 13:16:55 +0530

    2nd commit

--- SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Demo 2");
 	}
 
 }
